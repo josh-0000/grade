7a17f95c9885ed8487e425bc8af1b48dd7acc2ec
Joshua Gmys <joshuagmys17@icloud.com>
2023-07-08 17:30:08 -0400

    changing entity names and relationship names

---
+++ Task+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Task+CoreDataClass.swift
+//  grade
+//
+//  Created by Joshua Gmys on 7/8/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Task)
+public class Task: NSManagedObject {
+
+}
